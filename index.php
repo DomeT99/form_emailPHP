7fc8873b35d334070a193cadc005e1bf657a8eb9
DomeT99 <domenicotenace99@gmail.com>
2021-10-02 14:36:01 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Jquery -->
+    <script src="../email/Jquery/jquery-3.6.0.min.js"></script>
+
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="../email/Bootstrap/css/bootstrap-grid.min.css">
+    <link rel="stylesheet" href="../email/Bootstrap/css/bootstrap.min.css">
+
+    <!-- CSS -->
+    <link type="text/css" rel="stylesheet" href="../email/Style/index.css">
+    <title>E-mail form!</title>
+
+
+
+    <?php
+
+   
+    $emailTo = $_POST['emailTo'];
+    $object = $_POST['object'];
+    $bodyMsg = $_POST['body'];
+    $header = "From : Tufast";
+
+
+    if (mail($emailTo, $object, $bodyMsg, $header)) {
+
+        echo "Mail send!";
+    } else {
+
+
+        echo "Error";
+    }
+
+    ?>
+    
+
+</head>
+
+<body>
+    <div class="container-sm">
+        <form action="../email/index.php" method="POST">
+            <div class="container_tot row-cols-sm-6">
+                <span class="lbl_com">Enter the recipient's email</span>
+                <input class="txt_email" type="text" name="emailTo" id="emailTo" placeholder="E-mail to" autocomplete="false">
+
+                <span class="lbl_com">Enter the subject of the email</span>
+                <input class="txt_email" type="text" name="object" id="object" placeholder="Object" autocomplete="false">
+
+                <span class="lbl_com txt_body">Enter the body of the email</span>
+                <textarea class="body_email" name="body" id="body_email"></textarea>
+
+                <input class="btn_send" type="submit" value="Send">
+            </div>
+        </form>
+    </div>
+</body>
+
+</html>
